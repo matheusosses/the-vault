9a7b8ef4b07dced59b56cd0c2a921d00fe609b12
matheusosses <maths0ss3s@gmail.com>
2026-05-13 15:28:38 -0300

    chore: args para Game

--- src/main/java/br/com/matheusosses/the_vault/game/Game.java
+++ src/main/java/br/com/matheusosses/the_vault/game/Game.java
@@ -1,13 +1,17 @@
 package br.com.matheusosses.the_vault.game;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "games")
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Game {
 
     @Id
