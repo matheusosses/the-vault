3147c8c9b3aefd96e62a4891af2d3ec782504d60
matheusosses <maths0ss3s@gmail.com>
2026-05-19 13:52:14 -0300

    feat: documentação com swagger

---
+++ src/main/java/br/com/matheusosses/the_vault/infra/springdoc/SpringDocConfigurations.java
@@ -0,0 +1,32 @@
+package br.com.matheusosses.the_vault.infra.springdoc;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocConfigurations {
+
+    @Bean
+    public OpenAPI customOpenApi() {
+        return new OpenAPI()
+            .components(new Components()
+                .addSecuritySchemes("bearer-key",
+                    new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("bearer")
+                        .bearerFormat("JWT")))
+
+            .info(new Info()
+                .title("The Vault API")
+                .description("API Restful para gerenciamento de catálogos de jogos, avaliações e coleções pessoais.")
+                .contact(new Contact()
+                    .name("Matheus Osses")
+                    .email("maths0ss3s@gmail.com"))
+                .version("1.0.0"));
+    }
+}
